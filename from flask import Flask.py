42041b2e3cf2dd8f1da18421bb2eaebbd016f244
malasiaa <jose.jn@hotmail.com>
2024-09-15 17:05:59 +0100

    Create from flask import Flask.py

---
+++ from flask import Flask.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/trigger', methods=['GET'])
+def trigger():
+    print("Motion detected!")
+    # Call your function or run any script here
+    run_script()
+    return "OK", 200
+
+def run_script():
+    # Code to run your Python script
+    print("Running the Python script...")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000) #host='0.0.0.0' if all hosts can send the http request
