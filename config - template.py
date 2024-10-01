6ef235798ed1d56e44fcfc4bda61c9b9811b0610
malasiaa <jose.jn@hotmail.com>
2024-10-01 16:05:11 +0100

    Update config - template.py

--- config - template.py
+++ config - template.py
@@ -1,8 +1,8 @@
 #-----flask script
 
 # IPs allowed to send requests to flask server
-# (Template IPs)
-ALLOWED_IPS = ['192.168.69.69', '192.168.69.70']  # change to "None" if any IP can send a request 
+# (Template IPs, replace with your IP camera's stream URL, and with you pc's IP if you desire)
+ALLOWED_IPS = ['192.168.1.2', '192.168.1.3']  # change to "None" if any IP can send a request to flask server
 
 
 #-----model function python
@@ -11,9 +11,8 @@ ALLOWED_IPS = ['192.168.69.69', '192.168.69.70']  # change to "None" if any IP c
 TELEGRAM_bot_token = 'TELEGRAM:TOKEN'
 TELEGRAM_chat_id = 'CHATID12342'
 
-# Replace with your IP camera's stream URL
-# (Template IPs)
-IP_CAMERA_URL = 'http://192.168.69.71:80/stream'
+# Template IPs, replace with your IP camera's stream URL
+IP_CAMERA_URL = 'http://192.168.1.2:80/stream'
 
 # insert the path to the directory where images will be stored, classified with humans or not
 OUTPUT_IMAGES_PATH = "output_images"
